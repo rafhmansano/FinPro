9cdf35d966e5d6f8c197b060845a7b319c19cef1
rafhmansano <rafael.mansano@gmail.com>
2026-01-02 16:04:46 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        slate: {
+          850: '#151f32',
+          900: '#0f172a',
+          950: '#020617',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
